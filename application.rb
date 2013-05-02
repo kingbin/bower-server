cd9f52d3af2d0164fb707f6ec038a766aeb15c34
Chris <cblazek@armt.com>
2013-05-01 19:06:08 -0500

    server updates

--- application.rb
+++ application.rb
@@ -12,7 +12,49 @@ require 'json'
 require 'sequel'
 require 'sinatra/sequel'
 
+#require 'padrino-helpers'
 #require 'logger'
+
+#require "rack/oauth2/sinatra"
+
+#  register Rack::OAuth2::Sinatra
+#
+#  oauth.database = Mongo::Connection.new["bower_admin"]
+#  oauth.authenticator = lambda do |username, password|
+#    #user = User.find(username)
+#    #user if user && user.authenticated?(password)
+#    user = User.find_by_username(username)
+#    user.id if user && user.authenticated?(password)
+#  end
+#
+#
+#  get "/oauth/authorize" do
+#    if current_user
+#      render "oauth/authorize"
+#    else
+#      redirect "/oauth/login?authorization=#{oauth.authorization}"
+#    end
+#  end
+#
+#  post "/oauth/grant" do
+#    oauth.grant! "Superman"
+#  end
+#
+#  post "/oauth/deny" do
+#    oauth.deny!
+#  end
+#
+#  before do
+#    @current_user = User.find(oauth.identity) if oauth.authenticated?
+#  end
+#
+#  oauth_required "/packages/d"
+
+
+
+
+
+
 
 apps=File.expand_path(File.dirname(__FILE__))
 #set :database, 'sqlite:/'
@@ -57,8 +99,28 @@ class Package < Sequel::Model
   end
 end
 
+get '/' do
+ @msg = "ARMtech Bower Server"
+ @date = Time.now
+ erb :index
+end
+
 get '/packages' do
   Package.order(:name).all.to_json
+end
+
+get '/packages/:name' do
+  package  = Package[:name => params[:name]]
+
+  return 404 unless package
+
+  package.hit!
+  package.to_json
+end
+
+get '/packages/search/:name' do
+  packages = Package.filter(:name.ilike("%#{params[:name]}%")).order(:hits.desc)
+  packages.all.to_json
 end
 
 post '/packages' do
@@ -75,16 +137,14 @@ post '/packages' do
   end
 end
 
-get '/packages/:name' do
-  package  = Package[:name => params[:name]]
-
-  return 404 unless package
-
-  package.hit!
-  package.to_json
+delete '/packages' do
+  begin
+    package = Package[:name => params[:name]].destroy()
+    201
+  rescue Sequel::ValidationFailed
+    400
+  rescue Sequel::DatabaseError
+    406
+  end
 end
 
-get '/packages/search/:name' do
-  packages = Package.filter(:name.ilike("%#{params[:name]}%")).order(:hits.desc)
-  packages.all.to_json
-end
--- config.ru
+++ config.ru
@@ -1,2 +1,12 @@
+#require "rack/oauth2/sinatra"
+#require 'rack/oauth2'
+
 require './application'
+
+
+set :run, false
+set :environment, ENV['RACK_ENV'] || 'production'
+
 run Sinatra::Application
+
+
