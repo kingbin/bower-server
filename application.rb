05b74ef4d5f617042a0852d29e71123e6e40eac1
Chris <cblazek@armt.com>
2013-05-02 12:02:02 -0500

    removin trash

--- application.rb
+++ application.rb
@@ -14,8 +14,6 @@ require 'sinatra/sequel'
 
 #require 'padrino-helpers'
 #require 'logger'
-<<<<<<< HEAD
-=======
 
 #require "rack/oauth2/sinatra"
 
@@ -52,73 +50,7 @@ require 'sinatra/sequel'
 #
 #  oauth_required "/packages/d"
 
-
-
-
-
-
-
-apps=File.expand_path(File.dirname(__FILE__))
-#set :database, 'sqlite:/'
-set :database, "sqlite://#{apps}/db/bower.db.sqlite"
-#database.logger = Logger.new("#{apps}/log/db.log")
-puts "the packages table doesn't exist" if !database.table_exists?('packages')
-
-migration 'create packages' do
-  database.create_table :packages do
-    primary_key :id
-    String :name, :unique => true, :null => false
-    String :url, :unique => true, :null => false
-    DateTime :created_at
-    index :name
- end
-end
->>>>>>> cd9f52d3af2d0164fb707f6ec038a766aeb15c34
-
-#require "rack/oauth2/sinatra"
-
-#  register Rack::OAuth2::Sinatra
-#
-#  oauth.database = Mongo::Connection.new["bower_admin"]
-#  oauth.authenticator = lambda do |username, password|
-#    #user = User.find(username)
-#    #user if user && user.authenticated?(password)
-#    user = User.find_by_username(username)
-#    user.id if user && user.authenticated?(password)
-#  end
-#
-#
-#  get "/oauth/authorize" do
-#    if current_user
-#      render "oauth/authorize"
-#    else
-#      redirect "/oauth/login?authorization=#{oauth.authorization}"
-#    end
-#  end
-#
-#  post "/oauth/grant" do
-#    oauth.grant! "Superman"
-#  end
-#
-#  post "/oauth/deny" do
-#    oauth.deny!
-#  end
-#
-#  before do
-#    @current_user = User.find(oauth.identity) if oauth.authenticated?
-#  end
-#
-#  oauth_required "/packages/d"
-
-<<<<<<< HEAD
 set :root_path, File.dirname(__FILE__)
-=======
-  def validate
-    super
-#    errors.add(:url, 'is not correct format') if url !~ /^git:\/\//
-    errors.add(:url, 'is not correct format') if url !~ /^(git)|(https):\/\//
-  end
->>>>>>> cd9f52d3af2d0164fb707f6ec038a766aeb15c34
 
 require File.expand_path('../config/init', __FILE__)
 
@@ -176,13 +108,3 @@ delete '/packages' do
     406
   end
 end
-<<<<<<< HEAD
-
-
-
-
-
-
-=======
->>>>>>> cd9f52d3af2d0164fb707f6ec038a766aeb15c34
-
--- config.ru
+++ config.ru
@@ -1,16 +1,9 @@
-#require "rack/oauth2/sinatra"
-#require 'rack/oauth2'
 
 require './application'
-
 
 set :run, false
 set :environment, ENV['RACK_ENV'] || 'production'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> cd9f52d3af2d0164fb707f6ec038a766aeb15c34
 run Sinatra::Application
 
 
